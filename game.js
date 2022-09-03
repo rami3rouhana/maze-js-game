795bbebee880ae2c798c705b598aaba1095098cf
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-09-03 14:34:05 +0300

    Declaring start function

--- game.js
+++ game.js
@@ -1,4 +1,18 @@
 // Waiting till HTML page loads
 window.addEventListener('load', pageLoad = () =>{
+    // Variable Declaration
+    const start = document.getElementById("start");
+    const end = document.getElementById("end");
+    const boundaries = document.getElementsByClassName("boundary");
+    let game = false
+    
+    // Game Start
+    const gameStart = () => {
+        game = true
+        Array.from(boundaries).forEach( boundary => {
+            boundary.classList.remove("youlose");
+        });
+    }
+
 
 })
